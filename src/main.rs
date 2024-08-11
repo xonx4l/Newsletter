098f8b9a1f509143b9e130d5256efac34979eba3
xonx <119700621+xonx4l@users.noreply.github.com>
2024-08-11 20:37:55 +0530

    restructure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web,App,HttpRequest,HttpServer,Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("world");
@@ -6,28 +6,13 @@ async fn greet(req: HttpRequest) -> impl Responder {
 }
 
 #[tokio::main]
-async fn main () -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/",web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-      .bind("127.0.0.1:8000")?
-      .run()
-      .await
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
 
-
-#[cfg(test)]
-mod tests {
-    use crate::health_check;
-
-    #[tokio::test]
-    async fn health_check_succeeds() {
-        let response = health_check().await;
-        //this requires changing the return type of `health_check`
-        //from `impl Responder` to `HttoResponse` to compile
-        //You also need to import it with `use actix_web::HttpResponse`!
-        assert!(response.status().is_success())
-    }
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
