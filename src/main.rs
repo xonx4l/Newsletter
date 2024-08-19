102154082dbaadf06f93b4f307278dd5be7ad1f3
xonx4l <sk797119@gmail.com>
2024-08-19 23:30:41 +0530

    refactor tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
 use Newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run(std::net::TcpListener)?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
+
